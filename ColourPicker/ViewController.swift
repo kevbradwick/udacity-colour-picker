c2b4ab0df2d7a76dd7ec8f0d4c6cef44f7b18505
Kevin Bradwick <kevinbradwick@gmail.com>
2015-04-03 23:32:38 +0100

    built a colour picker

--- ColourPicker/ViewController.swift
+++ ColourPicker/ViewController.swift
@@ -10,16 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var colourView: UIView!
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        colourView.layer.borderColor = UIColor.blackColor().CGColor
+        colourView.layer.borderWidth = 1.0
+        updateColourView()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func sliderDidChange(sender: UISlider) {
+        println("Red: \(redSlider.value) Green: \(greenSlider.value) Blue: \(blueSlider.value)")
+        updateColourView()
     }
+    
+    func updateColourView() {
 
-
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+        
+        self.colourView.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1.0)
+    }
 }
 
